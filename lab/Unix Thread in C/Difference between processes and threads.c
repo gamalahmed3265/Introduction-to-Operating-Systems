092d9c1dda49e0dbcaa9974a7b96c300964e4fad
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-05-26 20:28:25 +0300

    Difference between processes and threads

---
+++ lab/Unix Thread in C/Difference between processes and threads.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+
+void* routine(){
+    printf("Test from Thread\n");
+    sleep(3);
+    printf("Ending Thread\n");
+}
+int main(int argc,char*argv[]){
+    int pid=fork();
+    if (pid==-1)
+        return -1;
+    print("Hello Process\n");
+
+    if(pid!=0)
+        wait(NULL);
+    return 0;
+}
