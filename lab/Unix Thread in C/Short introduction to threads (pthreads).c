348d8978f1232ed75921361b7f45f464143331e6
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-05-26 19:35:41 +0300

    Short introduction to threads (pthreads)

---
+++ lab/Unix Thread in C/Short introduction to threads (pthreads).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+
+void* routine(){
+    printf("Test from Thread\n");
+    sleep(3);
+    printf("Ending Thread\n");
+}
+int main(){
+    pthread_t t1;
+    pthread_t t2;
+    if (pthread_create(&t1,NULL,&routine,NULL)==0)
+        return 1;
+    if( pthread_create(&t2,NULL,&routine,NULL)==0)
+        return 2;
+    if (pthread_join(t1,NULL) !=0)
+        return 3;
+    if (pthread_join(t2,NULL) !=0)
+        return 4;
+    return 0;
+}
