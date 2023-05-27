1e2ac13a50dd08fefb01caf070ca3d94fcca2111
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-05-27 15:12:42 +0300

    add Waiting for processes to finish
    Waiting for processes to finish (using the wait function) in C

---
+++ lab/Unix Processes in C/Waiting for processes to finish (using the wait function) in C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc,char*argv[]){
+    pid_t pid;
+
+    pid = fork();
+    int n;
+    if (pid == 0) {
+        n=1;
+    } else {
+        n=6;
+    }
+    if (pid != 0) {
+        wait();
+    }
+    for (int i = n; i < n+1; i++){
+        printf("%d ",i);
+        fflush(stdout);
+    }
+    if (pid != 0) {
+        print("\n");
+    return 0;
+}
