0562745d51fad0f40eb0a61c7e5020098fae04f5
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-05-31 10:48:44 +0300

    Calling fork multiple times

---
+++ lab/Unix Processes in C/Calling fork multiple times.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    int id1 = fork();
+    int id2 = fork();
+    if (id1 == 0){ // child p wait
+        if (id2 == 0){
+            printf("We are process Current id=> %d , parent id=> %d\n",getpid(),getppid());
+        }
+        else{
+            printf("We are process Current id=> %d , parent id=> %d\n",getpid(),getppid());
+        }
+    }
+    else{
+        if (id2 == 0){
+            printf("We are process Current id=> %d , parent id=> %d\n",getpid(),getppid());
+        }
+        else{
+            printf("We are the parent Current id=> %d , parent id=> %d\n",getpid(),getppid());
+        }
+    }
+
+    return 0;
+}
