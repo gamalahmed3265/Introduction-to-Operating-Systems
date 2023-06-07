400da8b1bbef6f1960693a5cc5f463d1b5ac3ea5
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-06-07 17:23:36 +0300

    Update Executing commands in C.c

--- lab/Unix Processes in C/Executing commands in C.c
+++ lab/Unix Processes in C/Executing commands in C.c
@@ -9,7 +9,19 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]){
-    execlp("ping","ping","-c","3","google.com".NULL);
-    printf("Sucessful!");
+    int id=fork();
+    if(id==-1){
+        return 2;
+    }
+    if(id==0){
+        //child
+        execlp("ping","ping","-c","3","google.com",NULL);
+        printf("Sucessful!");
+    }
+    else{
+        //parent process
+        printf("Sucessful!\n");
+        printf("Some post processing goes here\n");
+    }
     return 0;
 }
