e580ec7272bcf7fa7e8359d26305eab14986df66
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-06-07 14:37:44 +0300

    Update Practical use case for fork and pipe in C.c

--- lab/Unix Processes in C/Practical use case for fork and pipe in C.c
+++ lab/Unix Processes in C/Practical use case for fork and pipe in C.c
@@ -35,7 +35,43 @@ int main(int argc, char *argv[]){
     for (i = start; i < end; i++){
         sum+=arr[i];
     }
+    // will output start-end 0-3 = sum=11
+    // will output start-end 3-7 = sum=16
     printf("Calc Practical  Sum is: %d\n",sum);
 
+    int fd[2];
+    // fd[0] - read
+    // fd[1] - write
+
+    if (pipe(fd)==-1){
+        printf("An error ocurred with opening the pip\n");
+        return 1;
+    }
+    if(id==0){
+        // close the pipe read 
+        close(fd[0]);
+        
+        // write d this value in fd
+        if(write(fd[1],&sum,sizeof(sum))==-1){
+            printf("An error occured with writing the pipe\n");
+            return 2;
+        }
+        // close the pipe write 
+        close(fd[1]);
+    }
+    else{
+        int totalSum;
+        // close the pipe write 
+        close(fd[1]);
+        if(read(fd[0],&totalSum,sizeof(totalSum))==-1){
+            printf("An error occured with reading the pipe\n");
+            return 2;
+        }
+        // close the pipe write 
+        close(fd[0]);
+        int totalSum=sum+totalSum;
+        printf("Sum in All Proces %d\n",totalSum);
+        wait(NULL);
+    }
     return 0;
 }
