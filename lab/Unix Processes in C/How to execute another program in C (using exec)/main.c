b6be388e1a72f3ae8d958ad0607bde25f07bf20c
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-06-07 19:33:04 +0300

    Add files via upload

--- lab/Unix Processes in C/How to execute another program in C (using exec)/main.c
+++ lab/Unix Processes in C/How to execute another program in C (using exec)/main.c
@@ -8,12 +8,32 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[],char* envp[]){
+    char* arr[]={
+        "ping",
+        "google.com",
+        NULL
+    };
+    
     _execl(
         "C:\\Windows\\System32\\PING.EXE",
         "C:\\Windows\\System32\\PING.EXE",
         "google.com",
         NULL
+        );
+    _execvp(
+        "ping",
+        arr,
+        
+    );
+    char* env[]={
+        "TEST= environment variable",
+        NULL
+    }
+    _execvpe(
+        "ping",
+        arr,
+        env
     );
     printf("Ping Finished executing\n");
     return 0;
