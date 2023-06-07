83a04bbc07d9509fb05c3d483e10b0721d4c753f
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-06-07 13:35:58 +0300

    Update Communicating between processes (using pipes) in C.c

--- lab/Unix Processes in C/Communicating between processes (using pipes) in C.c
+++ lab/Unix Processes in C/Communicating between processes (using pipes) in C.c
@@ -11,10 +11,15 @@ int main(int argc, char *argv[]){
     // fd[0] - read
     // fd[1] - write
 
-    if (pip(fd)==-1){
+    if (pipe(fd)==-1){
         printf("An error ocurred with opening the pip\n");
+        return 1;
     }
     int id=fork();
+    if (id==-1){
+        printf("An error occured with forking the process\n");
+            return 4;
+    }
     // child send verible to parent and compute value
     if (id==0){
         // close the pipe read 
@@ -23,7 +28,10 @@ int main(int argc, char *argv[]){
         printf("Input a number: ");
         scanf("%d",&x);
         // write d this value in fd
-        write(fd[1],&x,sizeof(int));
+        if(write(fd[1],&x,sizeof(int))==-1){
+            printf("An error occured with writing the pipe\n");
+            return 2;
+        }
         // close the pipe write 
         close(fd[1]);
     }
@@ -31,7 +39,12 @@ int main(int argc, char *argv[]){
         // close the pipe write 
         close(fd[1]);
         int y;
-        read(fd[0],&y,sizeof(int));
+        if(read(fd[0],&y,sizeof(int))==-1){
+            printf("An error occured with reading the pipe\n");
+            return 2;
+        }
+        //example we will y*3
+        y=y*3;
         // close the pipe write 
         close(fd[0]);
         printf("Get from child proces %d\n",y);
