6ebbabb86c636f74f003941cdd812603deb9b8a7
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-06-07 14:07:08 +0300

    Update Practical use case for fork and pipe in C.c

--- lab/Unix Processes in C/Practical use case for fork and pipe in C.c
+++ lab/Unix Processes in C/Practical use case for fork and pipe in C.c
@@ -7,17 +7,12 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]){
-    int arr[]={2,5,4,2,1,7,6,};
+    int arr[]={2,5,4,2,1,7,6};
     int sizeArr=sizeof(arr)/sizeof(int);
+    print("size of arr is: %d\n",sizeArr)
     int start,end;
     int fd[2];
-    // fd[0] - read
-    // fd[1] - write
-
-    if (pipe(fd)==-1){
-        printf("An error ocurred with opening the pip\n");
-        return 1;
-    }
+    
     int id=fork();
     if (id==-1){
         printf("An error occured with forking the process\n");
@@ -28,35 +23,10 @@ int main(int argc, char *argv[]){
         
         start=0;
         end=start+sizeArr/2;
-        // close the pipe read 
-        close(fd[0]);
-        int x;
-        printf("Input a number: ");
-        scanf("%d",&x);
-        // write d this value in fd
-        if(write(fd[1],&x,sizeof(int))==-1){
-            printf("An error occured with writing the pipe\n");
-            return 2;
-        }
-        // close the pipe write 
-        close(fd[1]);
     }
     else{
         start=sizeArr/2;
         end=sizeArr;
-
-        // close the pipe write 
-        close(fd[1]);
-        int y;
-        if(read(fd[0],&y,sizeof(int))==-1){
-            printf("An error occured with reading the pipe\n");
-            return 2;
-        }
-        //example we will y*3
-        y=y*3;
-        // close the pipe write 
-        close(fd[0]);
-        printf("Get from child proces %d\n",y);
     }
 
 
